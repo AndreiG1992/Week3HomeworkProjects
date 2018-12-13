f002ceff229ea82286a8677ff424374ab489698a
Andrei Gandila <andrei.gandila@yahoo.com>
2018-12-13 02:11:42 +0200

    Pushing the solution to GitHub

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,10 +6,104 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
+
+    public enum Gender
+    {
+        female = 0, male
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            int year, month, day;
+            string gender;
+
+            
+            do {
+                Console.Write("Year: "); year = int.Parse(Console.ReadLine());
+                if (year < 0 || year > DateTime.Today.Year)
+                    Console.WriteLine($"Introduceti un an valid intre 0 si {DateTime.Today.Year}");
+            } while (year<0 || year > DateTime.Today.Year);
+            do {
+                Console.Write("Month: "); month = int.Parse(Console.ReadLine());
+                if (month < 1 || month >12)
+                    Console.WriteLine($"Introduceti numarul corestunzator lunii [1-12]");
+            } while (month<1 || month>12);
+
+            do {
+                Console.Write("Day: "); day = int.Parse(Console.ReadLine());
+                if (day < 1 || day > 31)
+                    Console.WriteLine($"Introduceti numarul corestunzator zilei de nastere [1-31]");
+            } while (day<1 || day>31);
+
+            Console.Write("Gender[M/F]: "); gender = Console.ReadLine();
+
+
+            DateTime dateOfBirth = new DateTime(year, month, day);
+            DateTime currentDate = DateTime.Now;
+            //AgeCalculator method calling with two object's type arguments
+            AgeCalculator(dateOfBirth, currentDate);
+            int? gen;
+            if (gender == "M" || gender == "m")
+            {
+                gen = (int)Gender.male;
+            }
+            else if (gender == "F" || gender == "f")
+            {
+                gen = (int)Gender.female;
+            }
+            else gen = null;
+
+            if (gen.HasValue)
+            {
+                if (gen == 1)
+                {
+                    if (DateTime.Today.Year - dateOfBirth.Year >= 65)
+                        Console.WriteLine("Retired");
+                    else
+                        Console.WriteLine("He will retire at 65 years old");
+                }
+                if (gen == 0)
+                {
+                    {
+                        if (DateTime.Today.Year - dateOfBirth.Year >= 63)
+                            Console.WriteLine("Retired");
+                        else
+                            Console.WriteLine("She will retire at 63 years old");
+                    }
+                }
+            }
+            else Console.WriteLine("Invalid Gender!");
+
+
+
+
+        }
+        //method for calculation of complete age (years, months, days)
+        public static void AgeCalculator(DateTime dateOfBirth, DateTime currentDate)
+        {
+            int nrYears = 0;
+            int nrMonths = 0;
+            int nrDays = 0;
+            nrDays = currentDate.Day - dateOfBirth.Day;
+            nrMonths = currentDate.Month - dateOfBirth.Month;
+            nrYears = currentDate.Year - dateOfBirth.Year;
+            if (nrDays < 0)
+            {
+                nrDays += DateTime.DaysInMonth(currentDate.Year, currentDate.Month);
+                nrMonths--;
+                if (nrMonths < 0)
+                {
+                    nrMonths += 12;
+                    nrYears--;
+                }
+            }
+            if (nrMonths < 0)
+            {
+                nrMonths += 12;
+                nrYears--;
+            }
+            Console.WriteLine($"{nrYears} de ani, {nrMonths} luni, {nrDays} zile");
         }
     }
 }
---
+++ Project2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Andrei";
+            string lastName = "Gandila";
+
+            //string.Compare
+            Console.WriteLine(string.Compare(firstName, lastName));
+            Console.WriteLine(string.Compare(lastName, lastName));
+            Console.WriteLine(string.Compare(lastName, firstName));
+
+            //string.Concat
+            string fullName = string.Concat(firstName," ", lastName);
+            Console.WriteLine($"FullName: {fullName}");
+
+            //string.Insert
+            Console.WriteLine(fullName.Insert(7, "M. "));
+
+            //string.Split
+            string quote = "Amat victoria curam";
+            string[] quoteWords = quote.Split(' ');
+            foreach(var word in quoteWords)
+            {
+                Console.WriteLine(word);
+            }
+
+            //string.Contains
+            string[] urls = { "www.superbet.ro", "www.publicbet.ro", "www.luckybet.org" };
+            foreach(var str in urls)
+            {
+                if (str.Contains("bet"))
+                    Console.WriteLine("Malicious url: {0}", str);
+            }
+
+            //string.Trim
+            string text = "!!!!!!Ce greu e sa alegi un exemplu pentru textul de fata, chiar n-am inspiratie!!!!";
+            string trimmedText = text.Trim('!');
+            Console.WriteLine($"{text}\n{trimmedText}");
+
+            //string.ToUpper, string.ToLower
+            string anotherText = "Stringurile sunt destul de INTERESANTE";
+            Console.Write($"{anotherText.ToUpper()}\n{anotherText.ToLower()}\n");
+
+            //string.ToCharArray
+            char[] spelledFirstName = firstName.ToCharArray();
+            Console.WriteLine("Let's spell my first name: ");
+            foreach(var i in spelledFirstName)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
